326d9d2de3c42c1df49597e6ee7a76e6a3a3abbf
Axel <latiosaxe@gmail.com>
2017-07-31 21:52:47 -0500

    fillable

--- app/Client.php
+++ app/Client.php
@@ -12,6 +12,30 @@ class Client extends Model
         'regimen_de_ventas',
         'regimen_fiscal',
         'comprobante_de_domicilio',
-        'constancia_situacion_fiscal'
+        'razon_social',
+        'rfc',
+        'curp',
+        'titulo',
+        'complete_name',
+        'job',
+        'tel',
+        'cel',
+        'email',
+        'addres_stret',
+        'addres_colonia',
+        'addres_city',
+        'addres_cp',
+        'addres_municipio',
+        'addres_state',
+        'addres_country',
+        'addres_tel',
+        'addres_tel2',
+        'addres_cel',
+        'addres_website',
+        'addres_email',
+        'addres_contact',
+        'constancia_situacion_fiscal',
     ];
+
+
 }
