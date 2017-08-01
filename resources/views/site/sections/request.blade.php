13457e2b221432c2b80adfe51639e1da745d16db
Axel <latiosaxe@gmail.com>
2017-08-01 00:47:59 -0500

    Undefined

--- resources/views/site/sections/request.blade.php
+++ resources/views/site/sections/request.blade.php
@@ -542,6 +542,8 @@
             console.log(input);
             if(input.length == 0){
                 return 'Sin valor'
+            }else{
+                return input;
             }
         }
 
